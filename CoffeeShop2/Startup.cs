06d1d4652d924c508bc9f8a51fb240580e5a720c
ciorh <ciorhan.andreea@gmail.com>
2021-09-09 23:51:50 +0300

    Add project files.

---
+++ CoffeeShop2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoffeeShop2.Startup))]
+namespace CoffeeShop2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
